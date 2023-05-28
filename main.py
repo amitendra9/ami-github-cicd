784de68e485528dfb76e7475a2829693904d532c
Amitendra Singh <63096147+amitendra9@users.noreply.github.com>
2023-05-28 17:52:47 +0000

    add new func

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ def print_func():
     '''
     print('Hello World!!!')
 
+def second_func(a,b):
+    '''
+    This is second function for adding two number.
+    '''
+    return a+b
+
 if __name__ == "__main__":
     print_func()
+    num1 = 12
+    num2 = 13
+    sum = second_func(num1, num2)
+    print(sum)
